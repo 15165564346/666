ef83257217b2cc3a6b82683e84ea7cbc7bfd5a7a
15165564346 <111999105+15165564346@users.noreply.github.com>
2022-08-29 02:19:46 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,31 @@
+data = {
+    # 企微配置
+    # 企业ID
+    "corp_id": "ww6089d0b4e02faf5d",
+    # 企业应用Secret
+    "secret": "Tc09rmlFMjO8DsC7-6ttn2RmR86zAU-eHEE9y8DN-Yo",
+    # 企微应用AgentId，AgentId不需要使用引号
+    "agent_id": 1000002,
+    # 接收者的账号，多个接收者请用|分隔，例如 "user1ID|user2ID"，不填则默认发送应用可见范围所有人
+    "to_user": "",
+
+    # 信息配置
+    # 推送消息的标题
+    "title": "早上好，小宝！",
+    # 和风天气apikey
+    "weather_key": "3453048ec3ed465aa4555d64b5cabec4",
+    # 天行数据apikey，如不使用彩虹屁，可不填写
+    "tian_api": "d9540af79954acd4d7610182a33da7df",
+    # 所在地区，可为省，城市，区，县，同时支持国外城市，例如伦敦
+    "region": "临沭县",
+    # 生日1，前面加r代表农历生日
+    "birthday1": {"name": "笑宝", "birthday": "2005-08-05"},
+    # 生日2
+    "birthday2": {"name": "豪宝", "birthday": "2005-01-10"},
+    # 在一起的日子，格式同上
+    "love_date": "2021-10-08",
+    # 金句中文，如果为空，默认会读取金山的每日金句
+    "note_ch": "遇见即是上上签",
+    # 金句英文
+    "note_en": "TO meet is to sig on"
+}
---
+++ template.py
@@ -0,0 +1,21 @@
+txt = """
+\U0001F4C5 date
+\U0001F1E8\U0001F1F3 地区：region 
+\U00002601 天气：weather
+\U0001F534 最低气温：min_temp 
+\U0001F535 最高气温：max_temp 
+\U0001F536 当前气温：temp
+\U0001F343 当前风向：wind_dir 
+\U0001F332 pm2.5值：pm2p5
+\U0001F30F 空气质量：category 
+\U0001F305 日出时间：sunrise 
+\U0001F307 日落时间：sunset
+\U0001F49C 今天是我们恋爱的第love_day天 
+\U0001F382 birthday1 
+\U0001F382 birthday2
+
+\U0001F609 chp
+
+\U0001F1EA note_en
+\U0001F1E8 note_ch
+"""
